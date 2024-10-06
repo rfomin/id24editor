7546fad858b144a9a61cdf9d63577b3eeb98bc89
Roman Fomin <rfomin@gmail.com>
2024-10-06 13:28:54 +0700

    Implement numbers, formatting

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from ui_mainwindow import Ui_MainWindow
 from sbardef import SBarDef
 from omg import WAD
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -17,6 +18,7 @@ class MainWindow(QMainWindow):
         self.ui.graphicsView.resetTransform()
         self.ui.graphicsView.scale(s, s)
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
@@ -24,8 +26,8 @@ if __name__ == "__main__":
 
     model = QStringListModel()
     wad = WAD()
-    wad.from_file(r'c:\downloads\wads\DOOM2.WAD')
-    wad.from_file(r'c:\downloads\wads\id1.wad')
+    wad.from_file(r"c:\downloads\wads\DOOM2.WAD")
+    wad.from_file(r"c:\downloads\wads\id1.wad")
     lumps = wad.graphics + wad.patches + wad.sprites
     model.setStringList(lumps)
     window.ui.listView.setModel(model)
@@ -33,7 +35,7 @@ if __name__ == "__main__":
     scene = QGraphicsScene()
 
     window.ui.treeWidget.setColumnCount(2)
-    window.ui.treeWidget.setHeaderLabels(['Key', 'Value'])
+    window.ui.treeWidget.setHeaderLabels(["Key", "Value"])
     sbardef = SBarDef(scene, window.ui.treeWidget, window.ui.comboBox, wad)
     window.ui.comboBox.currentIndexChanged.connect(sbardef.draw)
     sbardef.draw(0)
--- src/sbardef.py
+++ src/sbardef.py
@@ -1,7 +1,13 @@
 from PySide6.QtCore import QPointF
-from PySide6.QtWidgets import (QComboBox, QGraphicsItem, QGraphicsPixmapItem,
-    QGraphicsScene, QGraphicsSceneMouseEvent,
-    QTreeWidget, QTreeWidgetItem)
+from PySide6.QtWidgets import (
+    QComboBox,
+    QGraphicsItem,
+    QGraphicsPixmapItem,
+    QGraphicsScene,
+    QGraphicsSceneMouseEvent,
+    QTreeWidget,
+    QTreeWidgetItem,
+)
 from PySide6.QtGui import QPixmap
 
 from PIL import Image
@@ -9,23 +15,26 @@ from PIL.ImageQt import ImageQt
 from omg import WAD
 import json
 
-SCREENWIDTH  = 320
+SCREENWIDTH = 320
 SCREENHEIGHT = 200
 
-sbe_h_left   = 0x00
+sbe_h_left = 0x00
 sbe_h_middle = 0x01
-sbe_h_right  = 0x02
-sbe_h_mask   = 0x03
-sbe_v_top    = 0x00
+sbe_h_right = 0x02
+sbe_h_mask = 0x03
+sbe_v_top = 0x00
 sbe_v_middle = 0x04
 sbe_v_bottom = 0x08
-sbe_v_mask   = 0x0C
+sbe_v_mask = 0x0C
+
 
 def clamp(smallest, largest, n):
     return max(smallest, min(n, largest))
 
+
 def cyanToAlpha(image: Image.Image) -> Image.Image:
-    image = image.convert('RGBA')
+    image = image.convert("RGBA")
+
     data = image.getdata()
     newData = []
     for item in data:
@@ -34,24 +43,47 @@ def cyanToAlpha(image: Image.Image) -> Image.Image:
         else:
             newData.append(item)
     image.putdata(newData)
+
     return image
 
+
+def lumpToPixmap(lump) -> QPixmap:
+    image = lump.to_Image()
+    image = cyanToAlpha(image)
+    return QPixmap(ImageQt(image))
+
+
+def imageToPixmap(image: Image.Image) -> QPixmap:
+    image = cyanToAlpha(image)
+    return QPixmap(ImageQt(image))
+
+
 class SBarElem(QGraphicsPixmapItem):
-    def __init__(self, elem: dict, x: int, y: int, screenheight: int,
-                 pixmap: QPixmap, tree: QTreeWidget):
+    def __init__(
+        self,
+        elem: dict,
+        x: int,
+        y: int,
+        screenheight: int,
+        pixmap: QPixmap,
+        tree: QTreeWidget,
+    ):
         super().__init__(pixmap)
         self.elem = elem
-        self.x_diff = x - elem['x']
-        self.y_diff = y - elem['y']
+        self.x_diff = x - elem["x"]
+        self.y_diff = y - elem["y"]
         self.screenheight = screenheight
-        self.setFlags(self.flags() | QGraphicsItem.ItemIsSelectable
-                      | QGraphicsItem.ItemIsMovable
-                      | QGraphicsItem.ItemSendsGeometryChanges)
+        self.setFlags(
+            self.flags()
+            | QGraphicsItem.ItemIsSelectable
+            | QGraphicsItem.ItemIsMovable
+            | QGraphicsItem.ItemSendsGeometryChanges
+        )
         self.tree = tree
 
     def loadElem(self, x, y):
-        self.elem['x'] = x - self.x_diff
-        self.elem['y'] = y - self.y_diff
+        self.elem["x"] = x - self.x_diff
+        self.elem["y"] = y - self.y_diff
 
         items = []
         for key, value in self.elem.items():
@@ -70,66 +102,150 @@ class SBarElem(QGraphicsPixmapItem):
         self.setPos(QPointF(x, y))
         return super().mouseReleaseEvent(event)
 
+
+class NumberFont:
+    def __init__(self, name: str):
+        self.name = name
+        self.numbers = []
+        self.maxwidth = 0
+        self.maxheight = 0
+        self.minus = None
+        self.percent = None
+
+    def addNumber(self, image: Image.Image):
+        self.numbers.append(cyanToAlpha(image))
+        self.maxwidth = max(self.maxwidth, image.width)
+        self.maxheight = max(self.maxheight, image.height)
+
+    def addMinus(self, image: Image.Image):
+        self.minus = cyanToAlpha(image)
+
+    def addPercent(self, image: Image.Image):
+        self.percent = cyanToAlpha(image)
+
+    def getPixmap(self, elem: dict, pct: bool) -> QPixmap:
+        maxlength = int(elem["maxlength"])
+        totalwidth = self.maxwidth * maxlength
+
+        if pct is True and self.percent is not None:
+            totalwidth += self.percent.width
+
+        image = Image.new("RGBA", (totalwidth, self.maxheight))
+        number = self.numbers[0]
+        for i in range(0, maxlength):
+            image.paste(number, (i * number.width, 0))
+
+        if pct is True and self.percent is not None:
+            image.paste(self.percent, (totalwidth - self.percent.width, 0))
+
+        return QPixmap(ImageQt(image))
+
+
 class SBarDef:
-    def __init__(self, scene: QGraphicsScene, tree: QTreeWidget,
-                 combo: QComboBox, wad: WAD):
+    def __init__(
+        self, scene: QGraphicsScene, tree: QTreeWidget, combo: QComboBox, wad: WAD
+    ):
         self.scene = scene
         self.tree = tree
         self.combo = combo
+
         self.wad = wad
         self.lumps = wad.graphics + wad.patches + wad.sprites
 
-        self.sbardef = json.loads(self.wad.data['SBARDEF'].data)
-        for statusbar in self.sbardef['data']['statusbars']:
-            if statusbar['fullscreenrender'] is True:
-                self.combo.addItem('Fullscreen')
+        self.sbardef = json.loads(self.wad.data["SBARDEF"].data)
+
+        for statusbar in self.sbardef["data"]["statusbars"]:
+            if statusbar["fullscreenrender"] is True:
+                self.combo.addItem("Fullscreen")
             else:
-                self.combo.addItem('Statusbar')
+                self.combo.addItem("Statusbar")
 
-    def addToScene(self, x, y, elem):
-        alignment = elem['alignment']
+        self.loadFonts()
 
-        if 'patch' in elem:
-            name = elem['patch']
+    def loadFonts(self):
+        self.numberfonts = []
+
+        for numberfont in self.sbardef["data"]["numberfonts"]:
+            font = NumberFont(numberfont["name"])
+            stem = numberfont["stem"]
+
+            for num in range(0, 10):
+                name = stem + "NUM" + str(num)
+                if name in self.lumps:
+                    font.addNumber(self.lumps[name].to_Image())
+
+            name = stem + "MINUS"
             if name in self.lumps:
-                image = self.lumps[name].to_Image()
-                image = cyanToAlpha(image)
-                pixmap = QPixmap(ImageQt(image))
-                item = SBarElem(elem, x, y, self.screenheight, pixmap, self.tree)
+                font.addMinus(self.lumps[name].to_Image())
 
-                width, height = image.size
-                if alignment & sbe_h_middle:
-                    x -= (width >> 1);
-                elif alignment & sbe_h_right:
-                    x -= width;
-                if alignment & sbe_v_middle:
-                    y -= (height >> 1)
-                elif alignment & sbe_v_bottom:
-                    y -= height
+            name = stem + "PRCNT"
+            if name in self.lumps:
+                font.addPercent(self.lumps[name].to_Image())
 
-                item.setPos(QPointF(x, y))
-                self.scene.addItem(item)
+            self.numberfonts.append(font)
 
-    def drawElem(self, x, y, elem):
-        x += elem['x']
-        y += elem['y']
+    def addToScene(self, x: int, y: int, elem: dict, pixmap: QPixmap):
+        alignment = elem["alignment"]
 
-        self.addToScene(x, y, elem)
+        item = SBarElem(elem, x, y, self.screenheight, pixmap, self.tree)
 
-        if elem['children'] is not None:
-            for child in elem['children']:
-                elem = next(iter(child.values()))
-                self.drawElem(x, y, elem)
+        if alignment & sbe_h_middle:
+            x -= pixmap.width() >> 1
+        elif alignment & sbe_h_right:
+            x -= pixmap.width()
+        if alignment & sbe_v_middle:
+            y -= pixmap.height() >> 1
+        elif alignment & sbe_v_bottom:
+            y -= pixmap.height()
+
+        item.setPos(QPointF(x, y))
+        self.scene.addItem(item)
+
+    def drawElem(self, x: int, y: int, elem: dict):
+        type = next(iter(elem))
+        values = next(iter(elem.values()))
+
+        x += values["x"]
+        y += values["y"]
+
+        if type == "graphic":
+            patch = values["patch"]
+            if patch in self.lumps:
+                lump = self.lumps[patch]
+                x -= lump.x_offset
+                y -= lump.y_offset
+                pixmap = lumpToPixmap(lump)
+                self.addToScene(x, y, values, pixmap)
+
+        elif type == "number" or type == "percent":
+            for font in self.numberfonts:
+                if font.name == values["font"]:
+                    pixmap = font.getPixmap(
+                        values, pct=True if type == "percent" else False
+                    )
+                    self.addToScene(x, y, values, pixmap)
+
+        elif type == "face":
+            lump = self.lumps["STFST00"]
+            if lump is not None:
+                x -= lump.x_offset
+                y -= lump.y_offset
+                pixmap = lumpToPixmap(lump)
+                self.addToScene(x, y, values, pixmap)
+
+        if values["children"] is not None:
+            for child in values["children"]:
+                self.drawElem(x, y, child)
 
     def draw(self, barindex: int):
         self.scene.clear()
 
-        statusbar = self.sbardef['data']['statusbars'][barindex]
-        self.screenheight = statusbar['height']
-        if statusbar['children'] is not None:
-            for child in statusbar['children']:
-                elem = next(iter(child.values()))
-                self.drawElem(0, 0, elem)
+        statusbar = self.sbardef["data"]["statusbars"][barindex]
+        self.screenheight = statusbar["height"]
+
+        if statusbar["children"] is not None:
+            for child in statusbar["children"]:
+                self.drawElem(0, 0, child)
 
         self.scene.setSceneRect(0, 0, SCREENWIDTH, self.screenheight)
 
